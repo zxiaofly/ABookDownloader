a12575854444a6f80f883e934300d6566e153315
HEIGE-PCloud <heige.pcloud@outlook.com>
2020-07-18 03:39:59 +0800

    make the code prettier~

--- ABookDownloader.py
+++ ABookDownloader.py
@@ -28,11 +28,10 @@ def downloader():
         with open(location, "wb") as f:
             f.write(r.content)
 
-def Abook_login(loginName, loginPassword):
-    """Log in and create a session"""
+def Abook_login(login_name, login_password):
     login_url = "http://abook.hep.com.cn/loginMobile.action"
     login_status_url = "http://abook.hep.com.cn/verifyLoginMobile.action"
-    login_data = {"loginUser.loginName": loginName, "loginUser.loginPassword": loginPassword}
+    login_data = {"loginUser.loginName": login_name, "loginUser.loginPassword": login_password}
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.64"}
     session.post(url=login_url, data=login_data, headers=headers)
     if session.post(login_status_url).json()["message"] == "已登录":
@@ -41,14 +40,12 @@ def Abook_login(loginName, loginPassword):
         print("Login failed, please try again.")
 
 def get_courses_info():
-    """get course info from web and save it to json"""
     course_info_url = "http://abook.hep.com.cn/selectMyCourseList.action?mobile=true&cur=1"
     with open(COURSES_INFO_FILE, 'w', encoding='utf-8') as file:
         json.dump(session.get(course_info_url).json(), file, ensure_ascii=False, indent=4)
     print("Courses fetched!")
 
 def load_courses_info():
-    """load courses info from local"""
     with open(COURSES_INFO_FILE, 'r', encoding='utf-8') as courses_info:
         courses_data: list = json.load(courses_info)[0]['myMobileCourseList']
         print('There are {} course(s) availaible.'.format(len(courses_data)))
@@ -57,7 +54,6 @@ def load_courses_info():
             course_id = eval('courses_data[{}]'.format(i))['courseInfoId']
             courses_list.append({'course_id': course_id, 'course_title': course_title})
 
-    ### Get and load chapter information
 def get_chapter_info(course_id):
     course_url = 'http://abook.hep.com.cn/resourceStructure.action?courseInfoId={}'.format(course_id)
     with open(str(course_id) + '.json', 'w', encoding='utf-8') as file:
