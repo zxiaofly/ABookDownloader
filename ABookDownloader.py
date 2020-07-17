e8978883cbcf1ba6e8aa251697f139b118c89223
HEIGE-PCloud <heige.pcloud@outlook.com>
2020-07-18 00:54:51 +0800

    get_course_info and load_course_info

--- ABookDownloader.py
+++ ABookDownloader.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -6,7 +7,9 @@ block_keyword = "style"
 url_base = "http://abook.hep.com.cn/ICourseFiles/"
 cnt = 1
 
+COURSES_INFO_FILE = "course_info.json"
 session = requests.session()
+courses_list = []
 
 def downloader(url, file_type, name):
     r = requests.get(url)
@@ -25,12 +28,30 @@ def Abook_login(loginName, loginPassword):
     else:
         print("Login failed, please try again.")
 
+def get_course_info():
+    """get course info from web and save it to json"""
+    course_info_url = "http://abook.hep.com.cn/selectMyCourseList.action?mobile=true&cur=1"
+    with open(COURSES_INFO_FILE, 'w', encoding='utf-8') as file:
+        json.dump(session.get(course_info_url).json(), file, ensure_ascii=False, indent=4)
+    print("Courses fetched!")
 
+def load_courses_info():
+    """load courses info from local"""
+    with open(COURSES_INFO_FILE, 'r', encoding='utf-8') as courses_info:
+        courses_data: list = json.load(courses_info)[0]['myMobileCourseList']
+        print('There are {} course(s) availaible.'.format(len(courses_data)))
+        for i in range(len(courses_data)):
+            course_title = eval('courses_data[{}]'.format(i))['courseTitle']
+            course_id = eval('courses_data[{}]'.format(i))['courseInfoId']
+            print('{}. {}'.format(i + 1, c_title))
+            courses_list.append({'serial': i + 1, 'course_id': course_id, 'course_title': course_title})
 
 if __name__ == "__main__":
     loginName = input("Please input loginName: ")
     loginPassword = input("Please input loginPassword: ")
     Abook_login(loginName, loginPassword)
+    get_course_info()
+    load_courses_info()
     # file = open("Sol.html", "r", encoding="utf-8")
     # for lines in file:
     #     if str(lines).find(keyword) != -1 and str(lines).find(block_keyword) == -1:
