9d31e599acf41226b352052607ce3794a58c5521
HEIGE-PCloud <heige.pcloud@outlook.com>
2020-09-04 18:17:17 +0100

    list view basic

--- ABook.py
+++ ABook.py
@@ -6,8 +6,10 @@ import requests
 
 from PySide2 import QtWidgets
 from PySide2 import QtCore
+from PySide2 import QtGui
 from UserLogin import ABookLogin
 from Settings import Settings
+from OSHandle import *
 
 
 class ABook(object):
@@ -62,11 +64,23 @@ class ABook(object):
         self.course_list[index]['chapter'] = chapter_list
         self.save_json_to_file(self.course_list_path, self.course_list)
 
+    def get_resource_info(self, course_id, chapter_id):
+        resource_list = self.course_list
+        for course in resource_list:
+            if str(course["courseInfoId"]) == course_id:
+                resource_list = course["chapter"]
+                break
+        for chapter in resource_list:
+            if str(chapter["id"]) == chapter_id:
+                resource_list = chapter["resource"]
+                break
+        return resource_list
+
     def save_json_to_file(self, path, data):
         with open(path, 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
-class CourseTreeView(QtWidgets.QWidget, ABook):
+class CourseTreeWidget(QtWidgets.QWidget, ABook):
 
     def __init__(self, path, settings, session):
         QtWidgets.QWidget.__init__(self)
@@ -77,6 +91,7 @@ class CourseTreeView(QtWidgets.QWidget, ABook):
         self.TreeWidget.setHeaderLabels(['Name', "Course ID", "Chapter ID"])
         self.TreeWidget.setAlternatingRowColors(True)
         self.TreeWidget.itemChanged.connect(self.checkbox_toggled)
+        self.TreeWidget.doubleClicked.connect(self.get_resource_info_from_item)
 
         self.download_button = QtWidgets.QPushButton("Download Selected")
         self.download_button.clicked.connect(self.download_selected)
@@ -85,11 +100,22 @@ class CourseTreeView(QtWidgets.QWidget, ABook):
         self.refresh_button.clicked.connect(self.refresh_course_list_tree)
 
 
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.TreeWidget)
-        layout.addWidget(self.download_button)
-        layout.addWidget(self.refresh_button)
-        self.setLayout(layout)
+        self.ListView = QtWidgets.QListView()
+        # self.ListView.setViewMode(QtWidgets.QListView.IconMode)
+        self.resource_list = QtGui.QStandardItemModel()
+        self.ListView.setModel(self.resource_list)
+        self.ListView.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.ListView.doubleClicked.connect(self.open_resource)
+
+        list_widget_layout = QtWidgets.QHBoxLayout()
+        list_widget_layout.addWidget(self.TreeWidget)
+        list_widget_layout.addWidget(self.ListView)
+
+        main_layout = QtWidgets.QVBoxLayout()
+        main_layout.addLayout(list_widget_layout)
+        main_layout.addWidget(self.download_button)
+        main_layout.addWidget(self.refresh_button)
+        self.setLayout(main_layout)
 
         for index in range(len(self.course_list)):
             self.create_tree(self.TreeWidget, self.course_list[index], 'course', index)
@@ -143,12 +169,51 @@ class CourseTreeView(QtWidgets.QWidget, ABook):
         for index in range(len(self.course_list)):
             self.create_tree(self.TreeWidget, self.course_list[index], 'course', index)
 
+    def get_resource_info_from_item(self):
+        course_id = self.sender().currentItem().text(1)
+        chapter_id = self.sender().currentItem().text(2)
+        if course_id != "None" and chapter_id != "None":
+            resource_list = self.get_resource_info(course_id, chapter_id)
+            self.resource_list.clear()
+            for resource in resource_list:
+                res_name = resource["resTitle"]
+                url_base = "http://abook.hep.com.cn/ICourseFiles/"
+                res_file_url = url_base + resource["resFileUrl"]
+                res_logo_url = url_base + resource["picUrl"]
+                logo = requests.get(res_logo_url).content
+                res_logo = QtGui.QImage()
+                res_logo.loadFromData(logo)
+                resource_item = QtGui.QStandardItem(res_name)
+                resource_item.setData(res_logo, QtCore.Qt.DecorationRole)
+                resource_item.setData(res_file_url, QtCore.Qt.ToolTipRole)
+                self.resource_list.appendRow(resource_item)
+
+    def open_resource(self):
+        item = self.resource_list.itemFromIndex(self.sender().currentIndex())
+        url = item.data(QtCore.Qt.ToolTipRole)
+        os.system("explorer " + url)
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, path, settings, session):
+        QtWidgets.QMainWindow.__init__(self)
+        course_tree_widget = CourseTreeWidget(path, settings, session)
+
+        main_layout = QtWidgets.QHBoxLayout()
+        main_layout.addWidget(course_tree_widget)
+        
+        main_frame = QtWidgets.QWidget()
+        main_frame.setLayout(main_layout)
+        self.setCentralWidget(main_frame)
+
+        self.setWindowTitle("ABookDownloader Dev")
+        self.resize(1920, 1080)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(os.sys.argv)
     user = ABookLogin('./temp/user_info.json')
     settings = Settings('./temp/settings.json')
-    abook = CourseTreeView('./temp/', settings, user)
+    abook = MainWindow('./temp/', settings, user)
     abook.show()
     sys.exit(app.exec_())
 
